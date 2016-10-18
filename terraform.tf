9e88d1b2f9586fcdb20dbda5531d56f943b7518c
manishas <manisha@shippable.com>
2016-10-18 10:43:56 -0700

    changed subnet and integration

--- terraform.tf
+++ terraform.tf
@@ -8,10 +8,10 @@ resource "aws_instance" "sampleInstances" {
 
   # ami = "${var.ecsAmi}"
   ami = "ami-0d729a60"
-  availability_zone = "us-east-1c"
+  availability_zone = "us-east-1b"
   instance_type = "t2.micro"
   #subnet_id = "subnet-84ea36cd"
-  subnet_id = "subnet-0def4d7a"
+  subnet_id = "subnet-5bfa2e76"
   associate_public_ip_address = true
   source_dest_check = false
 
