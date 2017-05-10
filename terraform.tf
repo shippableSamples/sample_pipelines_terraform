acc0ae187c805b2649a0a45cea6834e7ea95d873
Jatin Dogra <dogra.jatin5@gmail.com>
2017-05-10 12:43:05 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "sampleInstances" {
   count = 2
 
   # ami = "${var.ecsAmi}"
-  ami = "ami-79d45d19"
+  ami = "ami-11ca2d78"
   #The AMI specified above is a public Ubuntu image, backed by EBS. It was available on AWS at the time of
   #authoring this sample. You can also specify your own AMI image or use any other public EBS backed image if the
   #AMI used in the sample is decommissioned.
